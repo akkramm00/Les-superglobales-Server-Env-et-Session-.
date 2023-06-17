d039e36bf5c8139cff1bca2171a36e7cd19f3dcd
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-06-17 06:28:58 +0000

    add files traitement

--- index.php
+++ index.php
@@ -188,5 +188,26 @@ try {
    }
 ?>
 
+<h1> TRAITEMENT DES FICHIERS </h1>
+<h2> GET , POST ET FILE</h2>
+
+<?php
+if(isset($_GET['plat']) && !empty($_GET['plat'])) {
+  $plat = $_GET['plat'];
+} else {
+  $plat = 'Plat non défini';
+}
+?>
+
+<a href="?plat=pizza"> Plat 1 </a><br>
+<a href="?plat=salade"> Plat 2 </a><br>
+
+<?php
+echo '<br>';
+echo $plat;
+echo '</br>';
+?>
+
+
   </body>
 </html>
